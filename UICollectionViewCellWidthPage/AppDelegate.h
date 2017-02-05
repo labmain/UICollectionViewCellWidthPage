28ca4858370eb0d2296dc4c134455f5f309778d9
shun <wangshun8888@gmail.com>
2017-02-05 19:22:20 +0800

    Initial Commit

---
+++ UICollectionViewCellWidthPage/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UICollectionViewCellWidthPage
+//
+//  Created by 王顺 on 2017/2/5.
+//  Copyright © 2017年 shun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UICollectionViewCellWidthPage/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UICollectionViewCellWidthPage
+//
+//  Created by 王顺 on 2017/2/5.
+//  Copyright © 2017年 shun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
